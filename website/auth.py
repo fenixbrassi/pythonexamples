aee1b4602d8922f648e0ca8d29b965cab7755b26
Ernesto Ponce <ernesto.p11@gmail.com>
2024-01-05 12:51:09 -0600

    [python] agregando todo el codigo usado en los ejemplos de estudio.

---
+++ website/auth.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template,request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET','POST'])
+def login():
+    data = request.form
+    return render_template("login.html", pruebavar="pruebatexto solo para ejemplo" , pruebvar2="texto 22222")
+
+@auth.route('/logout')
+def logout():
+    return "<p>Mi pagina de logout</p>"
+
+@auth.route('/sign-up', methods=['GET','POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+    
+        if len(email) < 4:
+            flash('El correo electronico debe ser mayor a 3 caracteres.', category='error')
+        elif password1 != password2:
+            flash('Los passwords no coinciden.', category='error')
+        else:
+            new_user = User(email=email, first_name = first_name, password=generate_password_hash(password1) )
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Usuario registrado correctamente', category='success')
+            return redirect(url_for('views.home'))
+    return render_template("sign_up.html")
