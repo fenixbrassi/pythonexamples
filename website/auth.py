004ec2c123d77a21b585c7bcf5d4e7b2c913d91a
Ernesto Ponce <ernesto.p11@gmail.com>
2024-01-07 12:32:17 -0600

    [python] segundo stream.

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,29 @@
 from flask import Blueprint, render_template,request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required , logout_user, current_user
 from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
-    data = request.form
-    return render_template("login.html", pruebavar="pruebatexto solo para ejemplo" , pruebvar2="texto 22222")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Bienvenido' + user.first_name , category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+    return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Mi pagina de logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
@@ -22,6 +33,11 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
     
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Ese correo ya ha sido registrado.', category='error')
+            return render_template("sign_up.html")
+
         if len(email) < 4:
             flash('El correo electronico debe ser mayor a 3 caracteres.', category='error')
         elif password1 != password2:
@@ -31,5 +47,6 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash('Usuario registrado correctamente', category='success')
+            login_user(new_user, remember=True)
             return redirect(url_for('views.home'))
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
